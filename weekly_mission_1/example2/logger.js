706cdf317e14f4cb2b9c5728ca781872ef745e2a
Ricardo González Garza <rickygzz@hotmail.com>
2022-04-11 12:10:39 -0500

    Example 2.

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,17 @@
+// logger.js
+
+// Esta es una función que se guardará en este módulo como info
+exports.info = (message) => {
+    console.log(`info: ${message}`)
+}
+
+// Esta es una función que se guardará en este módulo como verbose
+exports.verbose = (message) => {
+    console.log(`verbose: ${message}`)
+}
+  
+/*
+const logger = require('./logger')
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
+* */
