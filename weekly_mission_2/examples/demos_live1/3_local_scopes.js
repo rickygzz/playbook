181e616f620e9ad9240e3072b8817ef5b41481b1
Ricardo González Garza <rickygzz@hotmail.com>
2022-04-20 16:01:38 -0500

    Add demos from live 1 (week2)

---
+++ weekly_mission_2/examples/demos_live1/3_local_scopes.js
@@ -0,0 +1,22 @@
+/*
+	Contexto local: definir variables dentro de funciones, fuera de ahí no
+    estarán disponibles.
+*/
+function my_function_to_keep_local_values() {
+    const my_age = 18
+    console.log(my_age)
+}
+  
+// console.log(my_age), Uncaught ReferenceError: gravity is not defined
+my_function_to_keep_local_values()
+  
+if (true){
+    const my_age = 27
+    console.log(my_age) // 27
+}
+// console.log(gravity), Uncaught ReferenceError: gravity is not defined
+
+for(let index = 0; index < 3; index++){
+    console.log(index)
+}
+// console.log(index), Uncaught ReferenceError: i is not defined
