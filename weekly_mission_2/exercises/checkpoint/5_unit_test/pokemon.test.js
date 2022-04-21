3179209cd596aca8d8fe9984255aa1a6b84bddf1
Ricardo González Garza <rickygzz@hotmail.com>
2022-04-21 07:59:38 -0500

    Requisitos 5. Unit Testing

---
+++ weekly_mission_2/exercises/checkpoint/5_unit_test/pokemon.test.js
@@ -0,0 +1,9 @@
+import Pokemon from './pokemon.js'
+
+test('1) Create a new object pokemon',
+    () => {
+        const myPokemon = new Pokemon('Pikachu')
+        expect(myPokemon.name).toBe('Pikachu')
+        // Corrige esta prueba
+    }
+);
