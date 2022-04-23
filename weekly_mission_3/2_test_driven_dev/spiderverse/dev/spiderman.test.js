d69adc8a821b457c036e23b76b92973b52e60bf2
Ricardo González Garza <rickygzz@hotmail.com>
2022-04-23 16:19:31 -0500

    Added Spiderman class and its test.

---
+++ weekly_mission_3/2_test_driven_dev/spiderverse/dev/spiderman.test.js
@@ -0,0 +1,16 @@
+const Spiderman = require('./../app/spiderman')
+
+describe("Unit test for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+        // 
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31,
+            "Andrew Garfield", 2, "Sony")
+        
+        // Validamos
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
+    });
+})
