0a894a1ff7f12e450843082cde6679c8efbd3f08
Ricardo González Garza <rickygzz@hotmail.com>
2022-04-24 16:31:08 -0500

    Update app.js to accept /explorersInNode

--- weekly_mission_3/6_express_server/express_basic/app.js
+++ weekly_mission_3/6_express_server/express_basic/app.js
@@ -18,6 +18,12 @@ app.get('/launchx', (req, res) => {
     res.send('Bienvenidos a launchx')
 })
 
+// Regresando un objeto
+app.get('/explorersInNode', (req, res) => {
+    const explorer = { name: "Explorer", msg: 'Hello' }
+    res.send(explorer)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
