46fee0cec0effc42b27fd23bd69a956d72dd9f26
Ricardo González Garza <rickygzz@hotmail.com>
2022-04-24 16:12:47 -0500

    Create app.js

---
+++ weekly_mission_3/6_express_server/express_basic/app.js
@@ -0,0 +1,18 @@
+// Usando objeto express
+const express = require('express')
+
+// App de Express
+const app = express()
+
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// Path inicial, responderá a la url localhost:3000
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
