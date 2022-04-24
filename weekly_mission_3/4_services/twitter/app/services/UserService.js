363fc2faa52a4cd65f34f5df01bdb8056b69a185
Ricardo González Garza <rickygzz@hotmail.com>
2022-04-24 14:02:13 -0500

    Requerimento 2 done.

--- weekly_mission_3/4_services/twitter/app/services/UserService.js
+++ weekly_mission_3/4_services/twitter/app/services/UserService.js
@@ -1,15 +1,23 @@
+const { info } = require("console")
 const User = require("../models/User")
 
 class UserService
 {
-    constructor()
-    {
-
-    }
-
     static create(id, username, name)
     {
         return new User(id, username, name, "Sin bio")
+    }
+
+    static getInfo(user)
+    {
+        var info = []
+
+        info.push(user.id)
+        info.push(user.username)
+        info.push(user.name)
+        info.push(user.bio)
+
+        return info
     }
 }
 
--- weekly_mission_3/4_services/twitter/test/services/UserService.test.js
+++ weekly_mission_3/4_services/twitter/test/services/UserService.test.js
@@ -9,4 +9,14 @@ describe("Tests for UserService", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
+
+    test("2. Get all user data in a list", () => {
+        const user = UserService.create(1, "rickygzz85", "Ricardo")
+        const userInfoInList = UserService.getInfo(user)
+
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("rickygzz85")
+        expect(userInfoInList[2]).toBe("Ricardo")
+        expect(userInfoInList[3]).toBe("Sin bio")
+    })
 })
