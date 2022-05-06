439fb06f3ec74bf0dedca77ce21e79195f25acec
Ricardo González Garza <rickygzz@hotmail.com>
2022-05-06 14:42:33 -0500

    Translated logger.js into English.

--- weekly_mission_1/example2/logger.js
+++ weekly_mission_1/example2/logger.js
@@ -1,16 +1,18 @@
 // logger.js
 
-// Esta es una función que se guardará en este módulo como info
+// This function will saved within this module as info
 exports.info = (message) => {
     console.log(`info: ${message}`)
 }
 
-// Esta es una función que se guardará en este módulo como verbose
+// This function will be saved within this module as verbose
 exports.verbose = (message) => {
     console.log(`verbose: ${message}`)
 }
   
 /*
+This code is placed within main.js to use logger module.
+
 const logger = require('./logger')
 logger.info('This is an informational message')
 logger.verbose('This is a verbose message')
