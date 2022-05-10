03e8ef12b9afa5ec161a149a3d99acd325fe967f
Ricardo González Garza <rickygzz@hotmail.com>
2022-05-10 18:16:09 -0500

    Updated app.js

--- weekly_mission_4/7_TrelloJS/app.js
+++ weekly_mission_4/7_TrelloJS/app.js
@@ -3,3 +3,21 @@ require('dotenv').config()
 if(!process.env.TOKEN && !process.env.KEY){
   throw new Error('There is no configuration with API key and token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `New Card ${new Date()}`
+
+trello.addCard(cardTitle,
+    "LaunchX Card Description",
+    "627acee9a04615291bcb215d",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	}
+);
