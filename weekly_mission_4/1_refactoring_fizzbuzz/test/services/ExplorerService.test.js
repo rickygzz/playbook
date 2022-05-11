3722152b89212637451bd728be4dd6181e303575
Ricardo González Garza <rickygzz@hotmail.com>
2022-05-10 20:21:31 -0500

    Updated ExplorerService test and FizzbuzzService tests.

--- weekly_mission_4/1_refactoring_fizzbuzz/test/services/ExplorerService.test.js
+++ weekly_mission_4/1_refactoring_fizzbuzz/test/services/ExplorerService.test.js
@@ -1,10 +1,22 @@
-const ExplorerService = require('../../lib/services/ExplorerService')
+const ExplorerService = require("../../lib/services/ExplorerService");
 
-describe("Tests para ExplorerService", () => 
+describe("ExplorerService tests", () => 
 {
-    test("Requerimiento 1: Calcular todos los explorers", () => {
-        const explorers = [{ mission: "node" }];
+    test("Requirement 1: filterByMission", () => {
+        const explorers = [{ mission: "node" }, { mission: "java" }, { mission: "node" }];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(1);
-    })
+        expect(explorersInNode.length).toBe(2);
+    });
+
+    test("Requirement 2: getAmountOfExplorersByMission", () => {
+        const explorers = [{ mission: "node" }, { mission: "java" }, { mission: "node" }];
+        const amount = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(amount).toBe(2);
+    });
+
+    test("Requirement 3: getExplorersUsernamesByMission", () => {
+        const explorers = [{ mission: "node", githubUsername: "name1" }, { mission: "java", githubUsername: "name2" }, { mission: "node", githubUsername: "name3" }];
+        const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(usernamesInNode).toStrictEqual(["name1", "name3"]);
+    });
 });
--- weekly_mission_4/1_refactoring_fizzbuzz/test/services/FizzbuzzService.test.js
+++ weekly_mission_4/1_refactoring_fizzbuzz/test/services/FizzbuzzService.test.js
@@ -1,11 +1,35 @@
 const FizzbuzzService = require('../../lib/services/FizzbuzzService')
 
-describe("Tests para FizzbuzzService", () => 
+describe("FizzbuzzService tests", () => 
 {
-    test("Requerimiento 1: Calcular todos los explorers", () => {
-        const explorers = [{ score: 3 }];
-        const validatedExplorer = FizzbuzzService.applyValidationInExplorer(explorers[0])
+    /**
+     * applyValidationInExplorer
+     */
+    test("Requirement 1: applyValidationInExplorer score=1", () => {
+        const explorer1 = {name: "Explorer1", score: 1};
+        const validatedExplorer = FizzbuzzService.applyValidationInExplorer(explorer1);
+
+        expect(validatedExplorer.trick).toBe(1);
+    });
+
+    test("Requirement 2: applyValidationInExplorer score=3", () => {
+        const explorer3 = {name: "Explorer3", score: 3};
+        const validatedExplorer = FizzbuzzService.applyValidationInExplorer(explorer3);
 
         expect(validatedExplorer.trick).toBe("FIZZ");
-    })
+    });
+
+    test("Requirement 3: applyValidationInExplorer score=5", () => {
+        const explorer5 = {name: "Explorer5", score: 5};
+        const validatedExplorer = FizzbuzzService.applyValidationInExplorer(explorer5);
+
+        expect(validatedExplorer.trick).toBe("BUZZ");
+    });
+
+    test("Requirement 4: applyValidationInExplorer score=15", () => {
+        const explorer15 = {name: "Explorer15", score: 15};
+        const validatedExplorer = FizzbuzzService.applyValidationInExplorer(explorer15);
+
+        expect(validatedExplorer.trick).toBe("FIZZBUZZ");
+    });
 });
